6cb9918eeef83b96071c369d5a2526c5f1c2d230
The third meow <jsheaandcats@gmail.com>
2019-01-02 14:19:38 +1300

    add simple (unsecure) hashing demo

---
+++ hashing.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+hash<string> hash_func;
+
+int main(int argc, char *argv[]) {
+
+	string pass;
+	cout << "Set Password: ";
+	cin >> pass;
+
+	unsigned long pass_hash = hash_func(pass);
+
+	/*
+	 * Another Day....
+	 */
+
+	cout << "Enter Password: ";
+
+	string pass_attempt;
+	cin >> pass_attempt;
+
+	if (hash_func(pass_attempt) == pass_hash)
+		cout << "Hello User" << endl;
+	else
+		cout << "Goodbye attacker" << endl;
+
+
+	return 0;
+}
+
